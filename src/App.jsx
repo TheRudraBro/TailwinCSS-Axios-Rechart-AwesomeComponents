55b9fe9523c2194691c6a8a339e23fb8be335b2a
TheRudraBro <rudramraj22@gmail.com>
2025-10-17 18:36:57 +0600

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import DaisyNav from './Components/DaisyNav/DaisyNav'
 import Navbar from './Components/DaisyNav/Navbar/Navbar'
+import PricingOptions from './Components/PricingOptions/PricingOptions'
 
 function App() {
   
@@ -16,7 +17,7 @@ function App() {
     
    </header>
    <main>
-    
+    <PricingOptions></PricingOptions>
    </main>
    <footer>
      
